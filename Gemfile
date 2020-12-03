097473c64672033194768e795bb6396ba2a403d4
Alexandre Tsuchiya <treakj@gmail.com>
2020-12-03 17:05:38 -0300

    com alterações

--- Gemfile
+++ Gemfile
@@ -66,3 +66,4 @@ end
 gem 'cloudinary', '~> 1.16.0'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    skill: Faker::Nation.language,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+  puts "created user"
+end
+
+20.times do
+  Job.create(
+    content: "translate #{Faker::Nation.language} to #{Faker::Nation.language}",
+    user_id: (0..9).to_a.sample
+  )
+  puts "created job"
+end
+
+50.times do
+  Proposal.create(
+    job_id: (0..19).to_a.sample,
+    status: 'pending',
+    price: (50..100).to_a.sample,
+    deadline: Faker::Date.forward(days: 23)
+  )
+  puts "created proposal"
+end
