3256273a7a931894a5f70b450db176b72049e97c
Joao Baumotte <jbaumotte@gmail.com>
2020-12-04 15:48:49 -0300

    Proposal Edit and Update OK

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,2 +1,27 @@
 class ProposalsController < ApplicationController
+
+  before_action :set_proposal, only: [:edit, :update]
+
+  def edit
+    @job = Job.find(params[:job_id])
+  end
+
+  def update
+    @proposal.update(proposal_status_params)
+
+    @job = Job.find(params[:job_id])
+    redirect_to @job
+  end
+
+  private
+
+  def proposal_status_params
+    params.require(:proposal).permit(:status)
+  end
+
+  def set_proposal
+    @proposal = Proposal.find(params[:id])
+    authorize @proposal
+  end
+
 end
