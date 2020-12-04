89d914ac8bf9c24e53569562737be533c122247d
Eluzai Santos <luexclusiva@gmail.com>
2020-12-04 16:05:34 -0300

    Created action destroy with the condition: only can be destroyed by his owner and if there is no proposal for her.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :jobs, only: %i[new create index show] do
-    resources :proposals, only: %i[create update edit]
+  resources :jobs, only: %i[new create index show destroy] do
+    resources :proposals, only: %i[create update edit index show destroy]
     resources :review, only: %i[new create]
   end
   get '/jobs/myjobs', to: 'jobs#my_jobs'
