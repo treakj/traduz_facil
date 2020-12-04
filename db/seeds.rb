fa13d335e06062380e3dff5161c9fa68601cc7b1
Alexandre Tsuchiya <treakj@gmail.com>
2020-12-04 09:22:38 -0300

    deixar aleatório a aplicação de proposal em relação ao job

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,6 +49,9 @@ end
   )
   puts "created job #{Job.last.content}"
 
+  # para que nem todos os jobs tenham alguma proposta
+  next unless (0..99).to_a.sample > 50
+
   Proposal.create(
     job_id: Job.last.id, # Job.find(Job.pluck(:id).sample).id,
     user_id: for_proposal, # Job.find(Job.pluck(:id).sample).id,
