63e6b81a1ec076fe7ec1ddfc7be2aca71519992e
Joao Baumotte <jbaumotte@gmail.com>
2020-12-04 23:05:19 -0300

    Created user controller#show and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -7,4 +8,7 @@ Rails.application.routes.draw do
     resources :review, only: %i[new create]
   end
   get '/jobs/myjobs', to: 'jobs#my_jobs'
+
+  # creating user/:id 
+  resources :users, only: [:show]
 end
