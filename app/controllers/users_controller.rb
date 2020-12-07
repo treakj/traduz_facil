b0a3f96142911dd81da2ee4d872251b596f01b83
Eluzai Santos <luexclusiva@gmail.com>
2020-12-07 19:23:37 -0300

    Inclusao do filtro de jobs por status.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,4 @@
 class UsersController < ApplicationController
-  
-  def myjobs
-    @myproposals = Proposal.where(user_id: params[:user_id], status: "accepted")
-    authorize @myproposals
-  end
 
   def show
     @user = User.find(params[:id])
@@ -11,15 +6,8 @@ class UsersController < ApplicationController
     @proposals = Proposal.where(user_id: @user.id)
     
     if params[:filter].present?
-      # @user_filter = User.find(params[:id]) # Tem lá fora
-      # @proposals = Proposal.where(status: params[:filter]) # Tem lá fora
-      # @proposals = Proposal.where(user_id: @user_filter.id).where(status: params[:filter])
       query = "proposals.user_id = ? AND proposals.status = ?"
       @jobs = Proposal.joins(:job).where(query, @user.id, params[:filter])
-    # else
-    #   @user = User.find(params[:id])
-    #   @jobs = Job.where(user_id: @user.id)
-    #   @proposals = Proposal.where(user_id: @user.id)
     end
     authorize @user
   end
